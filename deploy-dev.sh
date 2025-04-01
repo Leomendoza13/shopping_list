479ceaad8f161707b006d83e426030161ceed799
Leomendoza13 <leo.mendoza@epita.fr>
2025-04-01 16:00:42 +0200

    Add deploy and destroy script files

---
+++ deploy-dev.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+PROJECT_ID="recrutement-polyconseil"
+REGION="europe-west1"
+IMAGE_NAME="europe-west1-docker.pkg.dev/${PROJECT_ID}/shopping-list-repo-dev/myapp:latest"
+
+cd terraform/env/dev/setup/
+terraform init
+terraform apply
+cd ../
+terraform init
+terraform apply -target=module.storage.google_artifact_registry_repository.shopping_list_repo
+
+cd ../../../
+
+
+docker build -t ${IMAGE_NAME} .
+
+
+gcloud auth configure-docker europe-west1-docker.pkg.dev
+docker push ${IMAGE_NAME}
+
+
+cd terraform/env/dev/
+#terraform apply 
+
+SERVICE_URL=$(terraform output -raw service_url)
+echo "✅ Déploiement terminé ! Service disponible à l'adresse :"
+echo "${SERVICE_URL}"
