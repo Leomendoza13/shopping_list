2bd6cee9b35d246637f60ac12f72b3931ed38fef
Leomendoza13 <leo.mendoza@epita.fr>
2025-04-01 13:16:01 +0200

    add module database
    - Add database instance
    - Add database

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,1 +1,20 @@
 #modules/storage/main.tf
+
+resource "google_sql_database_instance" "sql_database_instance" {
+  name             = var.sql_database_instance_name
+  database_version = "POSTGRES_15"
+  region           = var.region
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection = false
+}
+
+resource "google_sql_database" "database" {
+  name     = var.database_name
+  instance = google_sql_database_instance.sql_database_instance.name
+}
--- terraform/modules/storage/variables.tf
+++ terraform/modules/storage/variables.tf
@@ -1,1 +1,20 @@
 #modules/storage/main.tf
+
+variable sql_database_instance_name {
+    type = string
+    description = "Name of the SQL database instance"
+    default = "main-instance"
+}
+
+variable region {
+    type = string
+    description = "The region of the deployment"
+    default = "europe-west1"
+}
+
+variable database_name {
+    type = string
+    description = "The name of the database"
+    default = "shopping-database"
+}
+
