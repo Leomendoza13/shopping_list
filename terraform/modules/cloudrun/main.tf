01a03bcdbacbfe036d49a0c3c52266adda45cd3a
Leomendoza13 <leo.mendoza@epita.fr>
2025-04-01 18:04:36 +0200

    Add cloudrun module
    -variables
    -resource

--- terraform/modules/cloudrun/main.tf
+++ terraform/modules/cloudrun/main.tf
@@ -1,1 +1,20 @@
 #modules/cloudrun/main.tf
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = var.cloud_run_name
+  location = var.location
+  deletion_protection = false
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    timeout = "300s"
+    
+    containers {
+      image = var.image_path
+
+      ports {
+        container_port = 8080
+      }
+    }
+  }
+}
--- terraform/modules/cloudrun/variables.tf
+++ terraform/modules/cloudrun/variables.tf
@@ -1,1 +1,25 @@
 #modules/cloudrun/variables.tf
+
+variable project_id {
+    type = string
+    description = "The id of the project"
+    default = "recrutement-polyconseil"
+}
+
+variable location {
+    type = string
+    description = "The location of the deployment"
+    default = "europe-west1"
+}
+
+variable cloud_run_name {
+    type = string
+    description = "The name of the cloud run"
+    default = "cloudrun-service"
+}
+
+variable image_path {
+    type = string
+    description = "The path of the image docker inside the registry"
+    default = "europe-west1-docker.pkg/recrutement-polyconseil/shopping-list-repo/app:latest"
+}
