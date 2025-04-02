8c017fd5e781b2c991e18c1b1b1d763a71565c45
Leomendoza13 <leo.mendoza@epita.fr>
2025-04-02 16:29:18 +0200

    pylint

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -1,1 +1,11 @@
 #env/prod/main.tf
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
--- terraform/env/staging/main.tf
+++ terraform/env/staging/main.tf
@@ -1,1 +1,11 @@
 #env/staging/main.tf
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
