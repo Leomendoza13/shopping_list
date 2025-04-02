8ec3eb5f8e7abf04ca7a2ff90a58b4b3348ef776
Leomendoza13 <leo.mendoza@epita.fr>
2025-04-02 16:04:33 +0200

    bandit issues resolved for app.py

--- src/app.py
+++ src/app.py
@@ -107,5 +107,6 @@ def test_db():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
+    host = os.environ.get("HOST", "127.0.0.1")
     init_db()
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host=host, port=port, debug=False)
